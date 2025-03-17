3b351fb19b544c1e3dc9647b203ac01c0bf4463f
fulleni <fulleni@hotmail.com>
2025-03-17 18:35:36 +0100

    feat: add more preferences options
    - Added font size options
    - Added saved headlines feature
    - Added content density option
    - Renamed favorite to followed

--- lib/src/ht_preferences_client.dart
+++ lib/src/ht_preferences_client.dart
@@ -39,107 +39,262 @@ abstract class HtPreferencesClient {
   /// storing the theme.
   Future<void> setTheme(ThemeMode theme);
 
-  /// Gets the user's favorite news source IDs.
+  /// Gets the user's followed news source IDs.
   ///
   /// Returns a list of source IDs.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
-  /// retrieving the favorite source IDs.
-  Future<List<String>> getFavoriteSourceIds();
+  /// retrieving the followed source IDs.
+  Future<List<String>> getFollowedSourceIds();
 
-  /// Sets the user's favorite news source IDs.
+  /// Sets the user's followed news source IDs.
   ///
   /// [sources] is a list of source IDs.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
-  /// storing the favorite source IDs.
-  Future<void> setFavoriteSourceIds(List<String> sources);
+  /// storing the followed source IDs.
+  Future<void> setFollowedSourceIds(List<String> sources);
 
-  /// Adds a news source to the user's favorites.
+  /// Adds a news source to the user's follows.
   ///
   /// [source] is the ID of the source to add.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
   /// adding the source.
-  Future<void> addFavoriteSource(String source);
+  Future<void> addFollowedSource(String source);
 
-  /// Removes a news source from the user's favorites.
+  /// Removes a news source from the user's follows.
   ///
   /// [source] is the ID of the source to remove.
   ///
   /// Throws a [SourceNotFoundException] if the source is not in the user's
-  /// favorites.
+  /// follows.
   /// Throws a [PreferencesStorageException] if an error occurs while
   /// removing the source.
-  Future<void> removeFavoriteSource(String source);
+  Future<void> removeFollowedSource(String source);
 
-  /// Gets the user's favorite category IDs.
+  /// Gets the user's followed category IDs.
   ///
   /// Returns a list of category IDs.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
-  /// retrieving the favorite category IDs.
-  Future<List<String>> getFavoriteCategoryIds();
+  /// retrieving the followed category IDs.
+  Future<List<String>> getFollowedCategoryIds();
 
-  /// Sets the user's favorite category IDs.
+  /// Sets the user's followed category IDs.
   ///
   /// [categories] is a list of category IDs.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
-  /// storing the favorite category IDs.
-  Future<void> setFavoriteCategoryIds(List<String> categories);
+  /// storing the followed category IDs.
+  Future<void> setFollowedCategoryIds(List<String> categories);
 
-  /// Adds a category to the user's favorites.
+  /// Adds a category to the user's follows.
   ///
   /// [category] is the ID of the category to add.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
   /// adding the category.
-  Future<void> addFavoriteCategory(String category);
+  Future<void> addFollowedCategory(String category);
 
-  /// Removes a category from the user's favorites.
+  /// Removes a category from the user's follows.
   ///
   /// [category] is the ID of the category to remove.
   ///
   /// Throws a [CategoryNotFoundException] if the category is not in the user's
-  /// favorites.
+  /// follows.
   /// Throws a [PreferencesStorageException] if an error occurs while
   /// removing the category.
-  Future<void> removeFavoriteCategory(String category);
+  Future<void> removeFollowedCategory(String category);
 
-  /// Gets the user's favorite event country IDs.
+  /// Gets the user's followed event country IDs.
   ///
   /// Returns a list of country IDs.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
-  /// retrieving the favorite country IDs.
-  Future<List<String>> getFavoriteEventCountryIds();
+  /// retrieving the followed country IDs.
+  Future<List<String>> getFollowedEventCountryIds();
 
-  /// Sets the user's favorite event country IDs.
+  /// Sets the user's followed event country IDs.
   ///
   /// [countries] is a list of country IDs.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
-  /// storing the favorite country IDs.
-  Future<void> setFavoriteEventCountryIds(List<String> countries);
+  /// storing the followed country IDs.
+  Future<void> setFollowedEventCountryIds(List<String> countries);
 
-  /// Adds an event country to the user's favorites.
+  /// Adds an event country to the user's follows.
   ///
   /// [country] is the ID of the country to add.
   ///
   /// Throws a [PreferencesStorageException] if an error occurs while
   /// adding the country.
-  Future<void> addFavoriteEventCountry(String country);
+  Future<void> addFollowedEventCountry(String country);
 
-  /// Removes an event country from the user's favorites.
+  /// Removes an event country from the user's follows.
   ///
   /// [country] is the ID of the country to remove.
   ///
   /// Throws a [CountryNotFoundException] if the country is not in the user's
-  /// favorites.
+  /// follows.
   /// Throws a [PreferencesStorageException] if an error occurs while
   /// removing the country.
-  Future<void> removeFavoriteEventCountry(String country);
+  Future<void> removeFollowedEventCountry(String country);
+
+  /// Removes all followed news source IDs.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// removing the source IDs.
+  Future<void> removeAllFollowedSourceIds();
+
+  /// Removes all followed category IDs.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// removing the category IDs.
+  Future<void> removeAllFollowedCategoryIds();
+
+  /// Removes all followed event country IDs.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// removing the country IDs.
+  Future<void> removeAllFollowedEventCountryIds();
+
+  /// Gets the user's preferred text size for headline articles.
+  ///
+  /// Returns the [HeadlineArticleTextSize].
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// retrieving the text size.
+  Future<HeadlineArticleTextSize> getHeadlineArticleTextSize();
+
+  /// Sets the user's preferred text size for headline articles.
+  ///
+  /// [textSize] is the [HeadlineArticleTextSize] to set.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// storing the text size.
+  Future<void> setHeadlineArticleTextSize(
+    HeadlineArticleTextSize textSize,
+  );
+
+  /// Gets the user's preferred font size for the app.
+  ///
+  /// Returns the [AppFontSize].
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// retrieving the font size.
+  Future<AppFontSize> getAppFontSize();
+
+  /// Sets the user's preferred font size for the app.
+  ///
+  /// [fontSize] is the [AppFontSize] to set.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// storing the font size.
+  Future<void> setAppFontSize(AppFontSize fontSize);
+
+  /// Gets the user's saved headlines.
+  ///
+  /// Returns a list of serialized headline strings.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// retrieving the saved headlines.
+  Future<List<String>> getSavedHeadlines();
+
+  /// Sets the user's saved headlines.
+  ///
+  /// [headlines] is a list of serialized headline strings.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// storing the saved headlines.
+  Future<void> setSavedHeadlines(List<String> headlines);
+
+  /// Adds a headline to the user's saved headlines.
+  ///
+  /// [headline] is the serialized headline string to add.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// adding the headline.
+  Future<void> addSavedHeadline(String headline);
+
+  /// Removes a headline from the user's saved headlines.
+  ///
+  /// [headline] is the serialized headline string to remove.
+  ///
+  /// Throws a [HeadlineNotFoundException] if the headline is not in the user's
+  /// saved headlines.
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// removing the headline.
+  Future<void> removeSavedHeadline(String headline);
+
+  /// Removes all headlines from the user's saved headlines.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// removing the headlines.
+  Future<void> removeAllSavedHeadlines();
+
+  /// Gets the user's preferred content density.
+  ///
+  /// Returns the [ContentDensity].
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// retrieving the content density.
+  Future<ContentDensity> getContentDensity();
+
+  /// Sets the user's preferred content density.
+  ///
+  /// [contentDensity] is the [ContentDensity] to set.
+  ///
+  /// Throws a [PreferencesStorageException] if an error occurs while
+  /// storing the content density.
+  Future<void> setContentDensity(ContentDensity contentDensity);
+}
+
+/// Defines the possible content densities.
+enum ContentDensity {
+  /// Compact content density.
+  compact,
+
+  /// Comfortable content density.
+  comfortable,
+}
+
+/// Thrown when trying to remove a headline that's not in the user's saved headlines.
+class HeadlineNotFoundException implements Exception {
+  /// Creates a [HeadlineNotFoundException].
+  HeadlineNotFoundException(this.headline);
+
+  /// The headline that was not found.
+  final String headline;
+
+  @override
+  String toString() {
+    return 'HeadlineNotFoundException: Headline not found in saved headlines: $headline';
+  }
+}
+
+/// Defines the possible text sizes for headline articles.
+enum HeadlineArticleTextSize {
+  /// Small text size.
+  small,
+
+  /// Medium text size.
+  medium,
+
+  /// Large text size.
+  large,
+}
+
+/// Defines the possible font sizes for the app.
+enum AppFontSize {
+  /// Small font size.
+  small,
+
+  /// Medium font size.
+  medium,
+
+  /// Large font size.
+  large,
 }
 
 /// Defines the possible theme modes.
@@ -196,7 +351,7 @@ class InvalidThemeModeException implements Exception {
   }
 }
 
-/// Thrown when trying to remove a source that's not in the user's favorites.
+/// Thrown when trying to remove a source that's not in the user's follows.
 class SourceNotFoundException implements Exception {
   /// Creates a [SourceNotFoundException].
   SourceNotFoundException(this.source);
@@ -210,7 +365,7 @@ class SourceNotFoundException implements Exception {
   }
 }
 
-/// Thrown when trying to remove a category that's not in the user's favorites.
+/// Thrown when trying to remove a category that's not in the user's follows.
 class CategoryNotFoundException implements Exception {
   /// Creates a [CategoryNotFoundException].
   CategoryNotFoundException(this.category);
@@ -224,7 +379,7 @@ class CategoryNotFoundException implements Exception {
   }
 }
 
-/// Thrown when trying to remove a country that's not in the user's favorites.
+/// Thrown when trying to remove a country that's not in the user's follows.
 class CountryNotFoundException implements Exception {
   /// Creates a [CountryNotFoundException].
   CountryNotFoundException(this.country);
