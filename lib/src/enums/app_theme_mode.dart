3caa9cbe17084ef577a4d52697297f2cb121ac9d
fulleni <fulleni@hotmail.com>
2025-04-09 12:18:57 +0100

    feat: add AppThemeMode enum
    - Defines system, light, dark modes
    - Enables theme switching

---
+++ lib/src/enums/app_theme_mode.dart
@@ -0,0 +1,13 @@
+/// {@template app_theme_mode}
+/// Represents the app's theme mode.
+/// {@endtemplate}
+enum AppThemeMode {
+  /// Use the system's theme mode.
+  system,
+
+  /// Use the light theme mode.
+  light,
+
+  /// Use the dark theme mode.
+  dark,
+}
