7a4d7f3e2aabef8c9c3df9e79eeda186437676b0
fulleni <fulleni@hotmail.com>
2025-04-09 12:19:07 +0100

    feat: add app theme name enum
    - Added grey theme option
    - Added red theme option
    - Added blue theme option

---
+++ lib/src/enums/app_theme_name.dart
@@ -0,0 +1,13 @@
+/// {@template app_theme_name}
+/// Represents the app's theme name.
+/// {@endtemplate}
+enum AppThemeName {
+  /// Use the grey theme.
+  grey,
+
+  /// Use the red theme.
+  red,
+
+  /// Use the blue theme.
+  blue,
+}
