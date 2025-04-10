e0973ca68d3acb0fd7aba0c04eb9e70dfed6e144
fulleni <fulleni@hotmail.com>
2025-04-10 09:33:26 +0100

    chore: export shared types from other packages

--- lib/ht_preferences_client.dart
+++ lib/ht_preferences_client.dart
@@ -1,7 +1,11 @@
 /// Abstract client for managing user preferences for the headlines toolkit app.
 library;
 
+export 'package:ht_categories_client/ht_categories_client.dart' show Category;
+export 'package:ht_countries_client/ht_countries_client.dart' show Country;
+export 'package:ht_headlines_client/ht_headlines_client.dart' show Headline;
 export 'package:ht_shared/ht_shared.dart' show PaginatedResponse;
+export 'package:ht_sources_client/ht_sources_client.dart' show Source;
 
 export 'src/enums/enums.dart';
 export 'src/exceptions.dart';
