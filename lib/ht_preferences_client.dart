c0e98a95b2581eceeb1abba38d8baa2e4b61e13c
fulleni <fulleni@hotmail.com>
2025-04-10 09:14:45 +0100

    chore: export shared types and enums
    - Exported PaginatedResponse
    - Exported enums and models
    - Exported exceptions

--- lib/ht_preferences_client.dart
+++ lib/ht_preferences_client.dart
@@ -1,4 +1,9 @@
 /// Abstract client for managing user preferences for the headlines toolkit app.
 library;
 
+export 'package:ht_shared/ht_shared.dart' show PaginatedResponse;
+
+export 'src/enums/enums.dart';
+export 'src/exceptions.dart';
 export 'src/ht_preferences_client.dart';
+export 'src/models/models.dart';
